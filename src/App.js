edfafb083606fcf479e9d1750d5e3132bcca7578
Melisa <melisaaltinsoy@gmail.com>
2019-08-05 19:00:41 +0200

    noise

--- src/App.js
+++ src/App.js
@@ -2,38 +2,46 @@ import React from "react"
 // import logo from "./logo.svg"
 import "./App.css"
 
-const NUM_ROWS = 2
+const NUM_ROWS = 40
 const N_SAMPLES = 50
 const length = 600
 
 function App() {
   const inputArray = Array(NUM_ROWS).fill(0)
   const xArray = Array(N_SAMPLES).fill(0)
-  const yArray = Array(N_SAMPLES).fill(0)
 
   const rows = inputArray.map((n, i) =>
     xArray.map((n, j) => {
       const x = 100 + j * (length / N_SAMPLES)
-      console.log({ j, x: j * (length / N_SAMPLES) })
-      const y = 100 + i * (length / N_SAMPLES) * (Math.random() - 0.5)
+      const y = 100 + i * (length / N_SAMPLES) + Math.random() * 10
       return [x, y]
     })
   )
 
-  console.log(inputArray, rows)
+  // console.log(
+  //   rows[0][0].join(","),
+  //   rows[0][1],
+  //   rows[0][0].join(","),
+  //   rows[0][1]
+  // )
 
   return (
-    <svg width={1200} height={1200}>
-      <rect x={0} y={0} width={1200} height={1200} fill="none" />
-      {rows.map((row, i) => (
-        <polyline
-          key={`${i}`}
-          points={`${row[0][0]},${row[0][1]} ${row[1][0]},${row[1][1]}`}
-          fill="none"
-          stroke="black"
-          strokeWidth="3"
-        />
-      ))}
+    <svg width={900} height={900}>
+      <rect x={0} y={0} width={900} height={900} fill="none" />
+      {rows.map((points, i) => {
+        return (
+          <polyline
+            key={`${i}`}
+            points={` 
+            ${points}
+            
+            `}
+            fill="none"
+            stroke="black"
+            strokeWidth="3"
+          />
+        )
+      })}
     </svg>
   )
 }
