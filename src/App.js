8c04aae109e0a2240032962914a04127981d5883
Melisa <melisaaltinsoy@gmail.com>
2019-08-09 15:24:49 +0200

    smooth linnes with d3.line

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 // import logo from "./logo.svg"
 import "./App.css"
+import * as d3 from "d3"
 
 const NUM_ROWS = 50
 const N_SAMPLES = 50
@@ -9,8 +10,13 @@ const length = 600
 function App() {
   const inputArray = Array(NUM_ROWS).fill(0)
   const xArray = Array(N_SAMPLES).fill(0)
+  const curve = d3
+    .line()
+    .x(d => d[0])
+    .y(d => d[1])
+    .curve(d3.curveBundle.beta(1.5))
 
-  const rows = inputArray.map((n, i) =>
+  const lines = inputArray.map((n, i) =>
     xArray.map((n, j) => {
       const x = 150 + j * (length / N_SAMPLES)
       const distanceToCenter = Math.abs(j * (length / N_SAMPLES) - length / 2)
@@ -23,26 +29,20 @@ function App() {
       return [x, y]
     })
   )
-  // console.log(
-  //   rows[0][0].join(","),
-  //   rows[0][1],
-  //   rows[0][0].join(","),
-  //   rows[0][1]
-  // )
-
-  console.log(rows)
 
   return (
     <script src="https://cdnjs.cloudflare.com/ajax/libs/d3/4.2.2/d3.min.js" />,
     (
       <svg width={900} height={900} fill="black">
         <rect x={0} y={0} width={900} height={900} fill="black" />
-        {rows.map((points, i) => {
+        {lines.map(line => {
           return (
-            <polyline
-              key={`${i}`}
-              points={` 
-            ${points.join(" ")}
+            <path
+              key={` 
+              ${line}
+              `}
+              d={` 
+            ${curve(line)}
             `}
               fill="black"
               stroke="white"
